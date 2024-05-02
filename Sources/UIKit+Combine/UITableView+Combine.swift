09311c7991620a999e706d61eb37cfa443a05750
Swagat Mishra <swagat.hbk@gmail.com>
2024-05-02 16:32:07 -0400

    Add support for all delegate functions

--- Sources/UIKit+Combine/UITableView+Combine.swift
+++ Sources/UIKit+Combine/UITableView+Combine.swift
@@ -11,13 +11,13 @@ import Combine
 public extension UITableView {
     
     struct TableViewDelegatePublisher: Publisher {
-        public typealias Output = (Any?, Any?)?
+        public typealias Output = Any?
         public typealias Failure = Never
         
         let tableView: UITableView
-        var action: ((Any) -> Any)? = nil
+        var action: ((Int) -> Any)? = nil
         
-        public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, (Any?, Any?)? == S.Input {
+        public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Any? == S.Input {
             let subscription =
             TableViewSubscription(
                 tableView: self.tableView,
@@ -28,16 +28,16 @@ public extension UITableView {
         }
     }
     
-    class TableViewSubscription<DelegateEventSubscriber: Subscriber>: NSObject, Subscription, UITableViewDelegate where DelegateEventSubscriber.Input == (Any?, Any?)?, DelegateEventSubscriber.Failure == Never {
+    class TableViewSubscription<DelegateEventSubscriber: Subscriber>: NSObject, Subscription, UITableViewDelegate where DelegateEventSubscriber.Input == Any?, DelegateEventSubscriber.Failure == Never {
         
         weak private var tableView: UITableView?
         private var subscriber: DelegateEventSubscriber?
-        private var action: ((Any) -> Any)?
+        private var action: ((Int) -> Any)?
         
         init(
             tableView: UITableView,
             subscriber: DelegateEventSubscriber? = nil,
-            action: ((Any) -> Any)?
+            action: ((Int) -> Any)?
         ) {
             self.tableView = tableView
             self.subscriber = subscriber
@@ -55,56 +55,56 @@ public extension UITableView {
         
         // MARK: - IndexPath -> Void
         public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         
         public func tableView(_ tableView: UITableView, willBeginEditingRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         public func tableView(_ tableView: UITableView, didEndEditingRowAt indexPath: IndexPath?) {
-            _ = subscriber?.receive(((indexPath, nil)))
+            _ = subscriber?.receive(indexPath)
         }
         
         public func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         public func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         
         public func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         
         public func tableView(_ tableView: UITableView, performPrimaryActionForRowAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         
         public func tableView(_ tableView: UITableView, didBeginMultipleSelectionInteractionAt indexPath: IndexPath) {
-            _ = subscriber?.receive((indexPath, nil))
+            _ = subscriber?.receive(indexPath)
         }
         
         // MARK: - IndexPath -> CGFloat
         public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
             guard 
                 let action = action,
-                let height = action(indexPath) as? CGFloat
+                let height = action(indexPath.row) as? CGFloat
             else { return tableView.estimatedRowHeight }
             return height
         }
         public func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
             guard 
                 let action = action,
-                let height = action(indexPath) as? CGFloat
+                let height = action(indexPath.row) as? CGFloat
             else { return tableView.estimatedRowHeight }
             return height
         }
@@ -113,42 +113,42 @@ public extension UITableView {
         public func tableView(_ tableView: UITableView, canFocusRowAt indexPath: IndexPath) -> Bool {
             guard 
                 let action = action,
-                let focus = action(indexPath) as? Bool
+                let focus = action(indexPath.row) as? Bool
             else { return false }
             return focus
         }
         public func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
             guard
                 let action = action,
-                let highlight = action(indexPath) as? Bool
+                let highlight = action(indexPath.row) as? Bool
             else { return false }
             return highlight
         }
         public func tableView(_ tableView: UITableView, selectionFollowsFocusForRowAt indexPath: IndexPath) -> Bool {
             guard
                 let action = action,
-                let focus = action(indexPath) as? Bool
+                let focus = action(indexPath.row) as? Bool
             else { return false }
             return focus
         }
         public func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
             guard
                 let action = action,
-                let shouldIndent = action(indexPath) as? Bool
+                let shouldIndent = action(indexPath.row) as? Bool
             else { return false }
             return shouldIndent
         }
         public func tableView(_ tableView: UITableView, canPerformPrimaryActionForRowAt indexPath: IndexPath) -> Bool {
             guard
                 let action = action,
-                let canPerform = action(indexPath) as? Bool
+                let canPerform = action(indexPath.row) as? Bool
             else { return false }
             return canPerform
         }
         public func tableView(_ tableView: UITableView, shouldBeginMultipleSelectionInteractionAt indexPath: IndexPath) -> Bool {
             guard
                 let action = action,
-                let multipleSelection = action(indexPath) as? Bool
+                let multipleSelection = action(indexPath.row) as? Bool
             else { return false }
             return multipleSelection
         }
@@ -156,29 +156,30 @@ public extension UITableView {
         // MARK: - IndexPath -> IndexPath
         public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
             guard let action = action else { return nil }
-            return (action(indexPath) as? IndexPath?) ?? nil
+            return (action(indexPath.row) as? IndexPath?) ?? nil
         }
+        
         public func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
             guard let action = action else { return nil }
-            return (action(indexPath) as? IndexPath?) ?? nil
+            return (action(indexPath.row) as? IndexPath?) ?? nil
         }
         
         // MARK: - UITableViewFocusUpdateContext -> Bool
-        public func tableView(_ tableView: UITableView, shouldUpdateFocusIn context: UITableViewFocusUpdateContext) -> Bool {
-            guard let action = action else { return false }
-            return (action(context) as? Bool) ?? false
-        }
+//        public func tableView(_ tableView: UITableView, shouldUpdateFocusIn context: UITableViewFocusUpdateContext) -> Bool {
+//            guard let action = action else { return false }
+//            return (action(context) as? Bool) ?? false
+//        }
         
         // MARK: - IndexPath -> UITableViewCell.EditingStyle
         public func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
             guard let action = action else { return .none }
-            return (action(indexPath) as? UITableViewCell.EditingStyle) ?? .none
+            return (action(indexPath.row) as? UITableViewCell.EditingStyle) ?? .none
         }
         
         // MARK: - IndexPath -> Int
         public func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
             guard let action = action else { return 0 }
-            return (action(indexPath) as? Int) ?? 0
+            return (action(indexPath.row) as? Int) ?? 0
         }
         
         // MARK: - Section Delegate methods
@@ -230,57 +231,151 @@ public extension UITableView {
             return height
         }
         
-//        public func tableView(_ tableView: UITableView, didUpdateFocusIn context: UITableViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
-//            <#code#>
-//        }
-//        
+        public func tableView(_ tableView: UITableView, didUpdateFocusIn context: UITableViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+            _ = subscriber?.receive((context, coordinator))
+        }
+        
         public func tableViewDidEndMultipleSelectionInteraction(_ tableView: UITableView) {
             _ = subscriber?.receive(nil)
         }
-//        public func tableView(_ tableView: UITableView, shouldSpringLoadRowAt indexPath: IndexPath, with context: any UISpringLoadedInteractionContext) -> Bool {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, willDisplayContextMenu configuration: UIContextMenuConfiguration, animator: (any UIContextMenuInteractionAnimating)?) {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, willEndContextMenuInteraction configuration: UIContextMenuConfiguration, animator: (any UIContextMenuInteractionAnimating)?) {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {
-//            <#code#>
-//        }
+        
+        public func tableView(_ tableView: UITableView, willDisplayContextMenu configuration: UIContextMenuConfiguration, animator: (any UIContextMenuInteractionAnimating)?) {
+            _ = subscriber?.receive((configuration, animator))
+        }
+        
+        public func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+            _ = subscriber?.receive((view, section))
+        }
+        
+        public func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+            _ = subscriber?.receive((view, section))
+        }
+        
+        public func tableView(_ tableView: UITableView, willEndContextMenuInteraction configuration: UIContextMenuConfiguration, animator: (any UIContextMenuInteractionAnimating)?) {
+            _ = subscriber?.receive((configuration, animator))
+        }
+        
+        public func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            guard
+                let action = action,
+                let swipeActions = action(indexPath.row) as? UISwipeActionsConfiguration
+            else { return nil }
+            return swipeActions
+        }
+        
+        public func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            guard
+                let action = action,
+                let swipeActions = action(indexPath.row) as? UISwipeActionsConfiguration
+            else { return nil }
+            return swipeActions
+        }
+        
+        public func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
+            _ = subscriber?.receive((view, section))
+        }
+        
+        public func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {
+            _ = subscriber?.receive((view, section))
+        }
+        
 //        public func tableView(_ tableView: UITableView, previewForDismissingContextMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
-//            <#code#>
+//            guard
+//                let action = action,
+//                let preview = action(configuration) as? UITargetedPreview
+//            else { return nil }
+//            return preview
 //        }
-//        public func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
-//            <#code#>
-//        }
-//        public func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
-//            <#code#>
-//        }
+        
+        public func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+            guard
+                let action = action,
+                let menu = action(indexPath.row) as? UIContextMenuConfiguration
+            else { return nil }
+            return menu
+        }
+        
+        public func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+            guard
+                let action = action,
+                let title = action(indexPath.row) as? String
+            else { return nil }
+            return title
+        }
+        
 //        public func tableView(_ tableView: UITableView, previewForHighlightingContextMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
-//            <#code#>
+//            guard
+//                let action = action,
+//                let preview = action(configuration) as? UITargetedPreview
+//            else { return nil }
+//            return preview
 //        }
-//        public func tableView(_ tableView: UITableView, willPerformPreviewActionForMenuWith configuration: UIContextMenuConfiguration, animator: any UIContextMenuInteractionCommitAnimating) {
-//            <#code#>
-//        }
+        
+        public func tableView(_ tableView: UITableView, willPerformPreviewActionForMenuWith configuration: UIContextMenuConfiguration, animator: any UIContextMenuInteractionCommitAnimating) {
+            _ = subscriber?.receive((configuration, animator))
+        }
+        
 //        public func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
-//            <#code#>
+//            guard
+//                let action = action,
+//                let indexPath = action((sourceIndexPath, proposedDestinationIndexPath)) as? IndexPath
+//            else { return IndexPath(item: 0, section: 0) }
+//            return indexPath
 //        }
+        
+        //        public func tableView(_ tableView: UITableView, shouldSpringLoadRowAt indexPath: IndexPath, with context: any UISpringLoadedInteractionContext) -> Bool {
+        //            guard
+        //                let action = action,
+        //                let load = action((indexPath, context)) as? Bool
+        //            else { return false }
+        //            return load
+        //        }
+    }
+}
+
+public extension UITableView {
+    enum TableViewDelegateActions {
+        case didSelectRow
+        case didDeselectRow
+        case willDisplayCell
+        case didEndDisplayingCell
+        case willBeginEditingRow
+        case didEndEditingRow
+        case didHighlightRow
+        case didUnHighlightRow
+        case accessoryButtonTappedForRow
+        case performPrimaryActionForRow
+        case didBeginMultipleSelectionInteraction
+        case didEndMultipleSelectionInteraction
+        case heightForRow(((Int) -> CGFloat)?)
+        case estimatedHeightForRow
+        case canFocusRow
+        case shouldHighlightRow
+        case selectionFollowsFocusForRow
+        case shouldIndentWhileEditingRow
+        case canPerformPrimaryActionForRow
+        case shouldBeginMultipleSelectionInteraction
+        case willSelectRow
+        case willDeselectRow
+        case editingStyleForRow
+        case indentationLevelForRow
+        case heightForFooterInSection
+        case heightForHeaderInSection
+        case estimatedHeightForHeaderInSection
+        case estimatedHeightForFooterInSection
+        case viewForHeaderInSection
+        case viewForFooterInSection
+        case disUpdateFocusIn
+        case willDisplayContextMenu
+        case willDisplayFooterView
+        case willDisplayHeaderView
+        case willEndContextMenuInteraction
+        case leadingSwipeActionsConfigurationForRow
+        case trailingSwipeActionsConfigurationForRow
+        case didEndDisplayingFooterView
+        case didEndDisplayingHeaderView
+        case contextMenuConfigurationForRow
+        case titleForDeleteConfirmationButtonForRow
+        case willPerformPreviewActionForMenu
     }
 }
