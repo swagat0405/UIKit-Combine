2b9aae1696cf7ba632f7951c60502afce6fae101
Swagat Mishra <swagat.hbk@gmail.com>
2024-05-02 10:06:03 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UIKit+Combine",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UIKit+Combine",
+            targets: ["UIKit+Combine"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UIKit+Combine"),
+        .testTarget(
+            name: "UIKit+CombineTests",
+            dependencies: ["UIKit+Combine"]),
+    ]
+)
